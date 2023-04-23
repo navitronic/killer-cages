c5a7ccf0775c9280e0bc18ad9ea6ce4a8f1524e8
Adrian Palmer <navitronic@gmail.com>
2023-04-23 16:06:55 +1000

    Rejigger things

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React, { ChangeEvent, useState } from 'react';
+import React, {ChangeEvent, useContext, useState} from 'react';
 import { combinations, range } from './utils';
+import {DisplayOptionsContext} from "./context/DisplayOptionsContext";
 
 interface CombinationProps {
   total: number;
@@ -73,44 +74,45 @@ function displayCombination(total: number, size: number, exclusions: number[], d
   return <Combination total={total} size={size} exclusions={exclusions} />;
 }
 
-function App() {
+function InnerApp() {
   const [total, setTotal] = useState<number>(10);
   const [size, setSize] = useState<number>(3);
   const [excStr, setExcStr] = useState<string>('');
-  const [showAll, setShowAll] = useState<boolean>(false);
+
+  const displayOptions = useContext(DisplayOptionsContext);
 
   const handleCheckboxChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setShowAll(e.target.checked);
+    displayOptions.setShowAll(e.target.checked);
   };
 
   return (
-    <div className="container mx-auto my-10">
-      <h1 className="text-3xl font-bold mb-10">Killer Cage Calculator 🧮</h1>
-      <div className="text-3xl mb-20">
-        <div>
-          <label>
-            Total:
-            <input className="border-4 p-3 m-1 mr-4 rounded-xl font-bold" type="number" max={45} min={1} name={'total'} value={total ?? ''} onChange={(e) => setTotal(Number(e.target.value))} />
-          </label>
-          <label>
-            Size:
-            <input className="border-4 p-3 m-1 mr-4 rounded-xl font-bold" type="number" max={9} min={1} name={'size'} value={size ?? ''} onChange={(e) => setSize(Number(e.target.value))} />
-          </label>
-          <label>
-            Exclusions:
-            <input className="border-4 p-3 m-1 mr-4 rounded-xl font-bold" type="text" name={'exclusions'} value={excStr ?? ''} onChange={(e) => setExcStr(e.target.value)} placeholder="Eg. 1 5" />
-          </label>
+      <div className="container mx-auto my-10">
+        <h1 className="text-3xl font-bold mb-10">Killer Cage Calculator 🧮</h1>
+        <div className="text-3xl mb-20">
+          <div>
+            <label>
+              Total:
+              <input className="border-4 p-3 m-1 mr-4 rounded-xl font-bold" type="number" max={45} min={1} name={'total'} value={total ?? ''} onChange={(e) => setTotal(Number(e.target.value))} />
+            </label>
+            <label>
+              Size:
+              <input className="border-4 p-3 m-1 mr-4 rounded-xl font-bold" type="number" max={9} min={1} name={'size'} value={size ?? ''} onChange={(e) => setSize(Number(e.target.value))} />
+            </label>
+            <label>
+              Exclusions:
+              <input className="border-4 p-3 m-1 mr-4 rounded-xl font-bold" type="text" name={'exclusions'} value={excStr ?? ''} onChange={(e) => setExcStr(e.target.value)} placeholder="Eg. 1 5" />
+            </label>
+          </div>
+          <div className="text-base">
+            <label>
+              <input type="checkbox" checked={displayOptions.showAll} onChange={handleCheckboxChange} className="mr-1" />
+              Show all numbers
+            </label>
+          </div>
         </div>
-        <div className="text-base">
-          <label>
-            <input type="checkbox" checked={showAll} onChange={handleCheckboxChange} className="mr-1" />
-            Show all numbers
-          </label>
-        </div>
+        {displayCombination(total, size, excStrToArr(excStr), displayOptions.showAll)}
       </div>
-      {displayCombination(total, size, excStrToArr(excStr), showAll)}
-    </div>
   );
 }
 
-export default App;
+export default InnerApp;
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import InnerApp from './App';
 import reportWebVitals from './reportWebVitals';
+import {DisplayOptionsContext} from "./context/DisplayOptionsContext";
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
+interface AppProps {
+  children: React.ReactNode;
+}
+
+const App = ({ children }: AppProps) => {
+  const [showAll, setShowAll] = useState<boolean>(false);
+
+  return (
+    <DisplayOptionsContext.Provider value={{showAll, setShowAll}}>
+      {children}
+    </DisplayOptionsContext.Provider>
+  )
+}
+
 root.render(
   <React.StrictMode>
-    <App />
+    <App>
+      <InnerApp/>
+    </App>
   </React.StrictMode>,
 );
 
