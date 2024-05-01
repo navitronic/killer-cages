f3a9c0d1948e29146a2c735570d09491a20a8529
Adrian Palmer <adrian.palmer@vista.com>
2024-05-01 10:57:03 +1000

    Disable storybook onboarder

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,7 @@ import type { StorybookConfig } from '@storybook/react-webpack5';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: ['@storybook/preset-create-react-app', '@storybook/addon-onboarding', '@storybook/addon-links', '@storybook/addon-essentials', '@chromatic-com/storybook', '@storybook/addon-interactions'],
+  addons: ['@storybook/preset-create-react-app', '@storybook/addon-links', '@storybook/addon-essentials', '@chromatic-com/storybook', '@storybook/addon-interactions'],
   framework: {
     name: '@storybook/react-webpack5',
     options: {},
