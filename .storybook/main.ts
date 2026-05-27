7aa05184bc978a05552b219518083ce3cb3aef6f
Adrian Palmer <adrian.palmer@vista.com>
2026-05-27 11:22:43 +1000

    Clean up Storybook build warnings

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,8 @@
 import type { StorybookConfig } from '@storybook/react-vite';
+import { mergeConfig } from 'vite';
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@chromatic-com/storybook', '@storybook/addon-interactions'],
   framework: {
     name: '@storybook/react-vite',
@@ -11,5 +12,11 @@ const config: StorybookConfig = {
     autodocs: 'tag',
   },
   staticDirs: ['../public'],
+  viteFinal: (config) =>
+    mergeConfig(config, {
+      build: {
+        chunkSizeWarningLimit: 1000,
+      },
+    }),
 };
 export default config;
