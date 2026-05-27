25629f4fb111aba2113bb29f056e070fb624ad4f
Adrian Palmer <adrian.palmer@vista.com>
2026-05-27 11:59:47 +1000

    Expand Cage stories

--- src/stories/Cage.stories.tsx
+++ src/stories/Cage.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { within, userEvent } from '@storybook/test';
 import '../index.css';
 import Cage from '../ui/Cage';
 
@@ -15,5 +16,45 @@ export const Default: Story = {
     total: 10,
     exclusions: '',
     inclusions: '',
+  },
+};
+
+export const WithFilters: Story = {
+  args: {
+    size: 3,
+    total: 10,
+    exclusions: '7',
+    inclusions: '1',
+  },
+};
+
+export const ConflictingFilters: Story = {
+  args: {
+    size: 3,
+    total: 10,
+    exclusions: '1',
+    inclusions: '1',
+  },
+};
+
+export const EmptyResults: Story = {
+  args: {
+    size: 3,
+    total: 10,
+    exclusions: '',
+    inclusions: '9',
+  },
+};
+
+export const ExcludedRow: Story = {
+  args: {
+    size: 3,
+    total: 10,
+    exclusions: '',
+    inclusions: '',
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    await userEvent.click(canvas.getByRole('button', { name: 'Toggle combination 1, 2, 7' }));
   },
 };
