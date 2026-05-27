cee59bc09352b9a132e144711493a7558036bf4a
Adrian Palmer <adrian.palmer@vista.com>
2026-05-27 12:28:48 +1000

    Constrain page width

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
   <React.StrictMode>
-    <div className="container mx-auto">
+    <div className="mx-auto max-w-[800px]">
       <h1 className="text-3xl font-bold my-10 px-4 sm:px-10">Killer Cage Calculator 🧮</h1>
       <InnerApp />
     </div>
